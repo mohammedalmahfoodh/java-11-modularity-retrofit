23749012b796df519572467ca1322c999cac54db
mohammed <mohammedalmahfoodh@yahoo.com>
2019-02-26 15:03:39 +0100

    fixes

--- com.mainModule/src/com/mainModule/MainModuleClass.java
+++ com.mainModule/src/com/mainModule/MainModuleClass.java
@@ -17,18 +17,7 @@ public class MainModuleClass {
         UserMenue userMenue=new UserMenue();
         userMenue.selectUserInput();
 
-       MyPostsService myPostsService=new MyPostsService();
-     /*   MyCommentsService myCommentsService =new MyCommentsService();
-        System.out.println(myPostsService.getAmessage());
-        for (Post p: myPostsService.getAllPosts()   ) {
-            System.out.println(p.getTitle());
-       }
-        LOGGER.info("emails loading...");
-        for (Comment comment: myCommentsService.getAllComments()   ) {
-            System.out.println(comment.getEmail());
-        }*/
 
-        System.out.println(myPostsService.getOnePost(1).getTitle());
     }
 
 }
